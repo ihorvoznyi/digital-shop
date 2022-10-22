034bef651ec6e74f0ee7016b81eec5abfe920b5f
Ihor Voznyi <devphase.sec@gmail.com>
2022-10-22 12:41:47 +0300

    TF-16: create Brand Module

---
+++ server/src/brand/brand.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { BrandService } from './brand.service';
+import { BrandController } from './brand.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Brand } from '../database/entities';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Brand])],
+  providers: [BrandService],
+  controllers: [BrandController],
+  exports: [BrandService],
+})
+export class BrandModule {}
