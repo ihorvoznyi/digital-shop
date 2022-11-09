07f618dbaa000cf82729684723fc2da28c25bf28
Ihor Voznyi <devphase.sec@gmail.com>
2022-11-09 20:04:46 +0200

    TF-41: create Cart

--- client/src/layouts/global-layout/navbar/components/Cart.tsx
+++ client/src/layouts/global-layout/navbar/components/Cart.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { observer } from 'mobx-react-lite';
 import { BsCart3 } from 'react-icons/bs';
+import { cartStore } from '../../../../store';
+import Format from '../../../../utils/Format';
 
 import './styles/Cart.scss';
+import { useNavigate } from 'react-router-dom';
 
 const imageLink = 'https://jabko.ua/image/cache/catalog/products/2022/04/081823/iphone11-black-select-2019-124x124.jpg';
 
 const Cart = () => {
+  const navigate = useNavigate();
+  const { format } = Format;
+  const size = cartStore.cart.length;
   return (
     <div className='navbar__cart navbar__cart_dropdown'>
-      <div className='navbar__cart-btn'>
-        <div className='navbar__cart-icon' data-products={'2'}>
+      <div className='navbar__cart-btn' onClick={() => navigate('/order-page')}>
+        <div
+          className={`navbar__cart-icon ${size ? 'navbar__cart-icon_not-empty' : ''}`}
+          data-products={size ? size : ''}
+        >
           <BsCart3 />
         </div>
 
@@ -17,40 +27,39 @@ const Cart = () => {
       </div>
 
       <ul className='navbar__cart-products dropdown_menu--animated dropdown_menu'>
-        <li className='navbar__cart-product'>
-          <img className='navbar__cart-product-image' src={imageLink} alt='iPhone 11' />
+        {!size
+          ? <li className='navbar__cart-empty'>Корзина пуста</li>
+          : cartStore.cart.map((item) => (
+            <Fragment key={item.product.id}>
+              <li className='navbar__cart-product'>
+                <img className='navbar__cart-product-image' src={imageLink} alt={item.product.name} />
 
-          <div className='navbar__cart-product-details'>
-            <div className='navbar__cart-product-name'>
-              Apple iPhone 11 64GB (Black)
-            </div>
+                <div className='navbar__cart-product-details'>
+                  <div className='navbar__cart-product-name'>
+                    {item.product.name}
+                  </div>
 
-            <div className='navbar__cart-product-price'>
-              19 190 грн
-            </div>
-          </div>
-        </li>
+                  <div className='navbar__cart-product-price'>
+                    {format(item.product.price)}
+                  </div>
+                </div>
 
-        <li className='navbar__cart-product'>
-          <img className='navbar__cart-product-image' src={imageLink} alt='iPhone 11' />
+                <div className='navbar__cart-remove' onClick={() => cartStore.removeFromCart(item.product.id)}>
+                  X
+                </div>
+              </li>
 
-          <div className='navbar__cart-product-details'>
-            <div className='navbar__cart-product-name'>
-              Apple iPhone 11 64GB (Black)
-            </div>
+              <div className='divider' />
+            </Fragment>
+          ))}
 
-            <div className='navbar__cart-product-price'>
-              19 190 грн
-            </div>
-          </div>
-        </li>
-
-        <div className='dropdown_menu-btn'>
-          <span>Зробити Замовлення</span>
-        </div>
+        {cartStore.cart.length ? (
+          <div className='dropdown_menu-btn' onClick={() => navigate('/order-page')}>
+            <span>Зробити Замовлення</span>
+          </div>) : ''}
       </ul>
     </div>
   );
 };
 
-export default Cart;
+export default observer(Cart);
